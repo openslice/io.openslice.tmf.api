edf6a2bd126d1e11862b5eb9ab9ef06f16689e95
Christos Tranoris <tranoris@gmail.com>
2023-07-29 15:49:07 +0300

    fixed authentication from swagger UI

--- src/main/java/io/openslice/tmf/configuration/SwaggerDocumentationConfig.java
+++ src/main/java/io/openslice/tmf/configuration/SwaggerDocumentationConfig.java
@@ -31,10 +31,12 @@ import io.swagger.v3.oas.annotations.security.OAuthFlow;
 import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.SpecVersion;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 
 /**
  * For implementing the callback and events, it might be useful to check the DDD pattern: https://www.baeldung.com/spring-data-ddd
@@ -46,7 +48,8 @@ import io.swagger.v3.oas.models.info.License;
 @jakarta.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-08-29T19:18:54.771Z")
 
 @Configuration
-@SecurityScheme(name = "security_auth", type = SecuritySchemeType.OAUTH2,
+@SecurityScheme(name = "security_auth", type = SecuritySchemeType.OAUTH2, bearerFormat = "JWT", 
+scheme = "bearer",
 flows = @OAuthFlows(authorizationCode = @OAuthFlow(
 		authorizationUrl = "${springdoc.oAuthFlow.authorizationUrl}"
 		, tokenUrl = "${springdoc.oAuthFlow.tokenUrl}", scopes = {
@@ -73,7 +76,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer scm633OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info().title("TMF 633 Service Catalog Management")
 						.description("## TMF API Reference: TMF633 - Service Catalog Management"
 								+ "### Release : 18.5-December 2018  Service Catalog API is one of Catalog Management API Family. Service Catalog API goal is to provide a catalog of services.   "
@@ -113,7 +116,7 @@ public class SwaggerDocumentationConfig {
 		
 		
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 	              .info(new Info().title("TMF 620 Product Catalog Management")
 	            		  .description("## TMF API Reference: TMF620 - Product Catalog Management  ### Release : 19.0 - June 2019  Product Catalog API is one of Catalog Management API Family. Product Catalog API goal is to provide a catalog of products.   ### Operations Product Catalog API performs the following operations on the resources : - Retrieve an entity or a collection of entities depending on filter criteria - Partial update of an entity (including updating rules) - Create an entity (including default values and creation rules) - Delete an entity - Manage notification of events")
 	                      
@@ -150,7 +153,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer scm622OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info().title("TMF 622 Product Ordering")
 						.description( "TMF API Reference : TMF 622 - Product Ordering Management**\\\\n\\\\n**"
 								+ "Release : 19.0 - June 2019**\\\\n\\\\nThe Product Ordering API provides a standardized mechanism for placing a product order with all of the necessary order parameters.")
@@ -186,7 +189,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer rcm634OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info().title("TMF 634 Resource Catalog Management")
 						.description("## TMF API Reference: TMF 634 Resource Catalog Management")
 						.version("4.0.0")
@@ -220,7 +223,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer sim638OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info().title("TMF 638 Service Inventory Management")
 						.description("## TMF API Reference : TMF 638 - Service Inventory Management v4 Release 18.5 - Dec 2018  The intent of this API is to provide a consistent/standardized mechanism to query and manipulate the Service inventory.  The Service Inventory API can be used to query the service instances for a customer via Self Service Portal or the Call Centre operator can query the service instances on behalf of the customer while a customer may have a complaint or a query. Note: Only the CustomerFacingServices instances will be presented to the customer.  The Service Inventory API can be called by the Service Order Management to create a new service instance/ update an existing service instance in the Service Inventory.  ### Service resource Service is an abstract base class for defining the Service hierarchy. All Services are characterized as either being possibly visible and usable by a Customer or not. This gives rise to the two subclasses of  Service: CustomerFacingService and ResourceFacingService.  ### Service Inventory API performs the following operations on service  : - Retrieve  a service or a collection of services depending on filter criteria - Partial update of a service (including updating rules) - Create a service (including default values and creation rules and for administration users only) - Delete a service (for administration users only) - Notification of events on service  Copyright (c)TM Forum 2018. All Rights Reserved.")
 						.version("4.0.0")
@@ -258,7 +261,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer so641OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info().title("TMF 641 - Service Ordering Management ")
 						.description("## TMF API Reference : TMF 641 - Service Ordering Management  ## Release : 18.5 - Dec 2018  The Service Order API provides a standardized mechanism for managing Service Order, a type of order which can be used to place an order between a customer and a service provider or between a service provider and a partner and vice versa.  ### Service Order resource A service order will describe a list of service order items.  A service order item references an action on an existing  or  future  service.  By  service  we  designed  Customer Facing  Service  (CFS)  as  well  as  Resource Facing Service (RFS). From a component perspective, a service order should be available - from a Service Orchestration Component (and it could mix CFS and RFS) - from an Infrastructure Control & Management component (and it would have only RFS)  ### Service Order API performs the following operations on service order : - Retrieval of a service order or a collection of service orders depending on filter criteria - Partial update of a service order (including updating rules) - Creation of a service order (including default values and creation rules) - Deletion of service order (for administration purposes) - Notification of events on Service order  Copyright © TM Forum 2018. All Rights Reserved")
 						.version("4.0.0")
@@ -294,7 +297,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer sqm657OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info().title("TMF 657 - Service Quality Management")
 						.description("This is Swagger UI environment generated for the TMF Service Quality Management specification")
 						.version("2.0.0")
@@ -331,7 +334,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer prm669OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 )
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth"))
 				.info(new Info().title("TMF 669 - Party Role Management")
 						.description("This is Swagger UI environment generated for the TMF Party Role Management specification")
 						.version("2.0.0")
@@ -368,7 +371,8 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer pm632OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth"))
+				
 				.info(new Info().title("TMF 632 - API Party")
 						.description("## TMF API Reference : TMF 632 - Party   ### Release : 19.0   The party API provides standardized mechanism for party management such as creation, update, retrieval, deletion and notification of events. Party can be an individual or an organization that has any kind of relation with the enterprise. Party is created to record individual or organization information before the assignment of any role. For example, within the context of a split billing mechanism, Party API allows creation of the individual or organization that will play the role of 3 rd payer for a given offer and, then, allows consultation or update of his information.  ### Resources - Organization - Individual - Hub  Party API performs the following operations : - Retrieve an organization or an individual - Retrieve a collection of organizations or individuals according to given criteria - Create a new organization or a new individual - Update an existing organization or an existing individual - Delete an existing organization or an existing individual - Notify events on organizatin or individual")
 						.version("4.0.0")
@@ -380,7 +384,8 @@ public class SwaggerDocumentationConfig {
 						.url("https://www.tmforum.org/oda/open-apis/table"));	  	                  
 	  }
 	
-  @Bean
+
+@Bean
   public GroupedOpenApi pm632(){
 	 
 	  SpringDocUtils.getConfig().replaceWithClass(java.time.LocalDate.class, java.sql.Date.class);
@@ -404,7 +409,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer am651OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info().title("TMF 651 - Agreement Management ")
 						 .description("This is Swagger UI environment generated for the TMF Agreement Management specification")
 						.version("2.0.0")
@@ -442,7 +447,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer rom652OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info()
 						.title("TMF 652 Resource Order Management-v4.0.0")
 						.description("This is Swagger UI environment generated for the TMF 652-Resource Order Management-v4.0.0 specification")
@@ -479,7 +484,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer am666OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info()
 						.title("TMF 666 Account Management")
 						.description("This is Swagger UI environment generated for the TMF Account Management specification")
@@ -516,7 +521,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer ri639penAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info()
 						 .title("TMF 639 API Resource Inventory Management")
 						 .description("## TMF API Reference: TMF639 - Resource Inventory   ### Release : 19.5 - December 2019  Resource Inventory  API goal is to provide the ability to manage Resources.  ### Operations Resource Inventory API performs the following operations on the resources : - Retrieve an entity or a collection of entities depending on filter criteria - Partial update of an entity (including updating rules) - Create an entity (including default values and creation rules) - Delete an entity (for administration purposes) - Manage notification of events")
@@ -555,7 +560,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer sac640penAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info()
 						 .title("TMF 640 API Service Activation and Configuration")
 						 .description("## TMF API Reference: TMF640 - Service Activation and Configuration  ### Release : 18.5 - December 2018  Service Activation and Configuration API goal is to provide the ability to activate and configure Services.  ### Operations Service Activation and Configuration API performs the following operations on the resources : - Retrieve an entity or a collection of entities depending on filter criteria - Partial update of an entity (including updating rules) - Create an entity (including default values and creation rules) - Delete an entity (for administration purposes) - Manage notification of events")
@@ -593,7 +598,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer cm629penAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info()
 						 .title("TMF 629 Customer Management-v4.0.0")
 
@@ -633,7 +638,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer fi691OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info()
 						 .title("TMF 691 Federated ID")
 						 .description("This is Swagger UI environment generated for the TMF FederatedID specification")
@@ -671,7 +676,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer am642OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info()
 						 .title("TMF 642 Alarm Management")
 						 .description("## TMF API Reference: TMF642 Alarm Management API")
@@ -709,7 +714,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer stm653OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info()
 						 .title("TMF 653 Service Test Management")
 						 .description("## TMF API Reference: TMF653 - Service Test Management  ### Release : 20.0 - April 2020  Service Test Management API goal is to provide the ability to manage tests of provisioned Services.   ### Resource  - ServiceTest  - ServiceTestSpecification  ### Operations Service Test Management API performs the following operations on the resources : - Retrieve an entity or a collection of entities depending on filter criteria - Partial update of an entity (including updating rules) - Create an entity (including default values and creation rules) - Delete an entity (for administration purposes) - Manage notification of events")
@@ -747,7 +752,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer lcmOpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info()
 						 .title("Openslice LCMRules specification API")
 			            .description("This is Swagger UI environment for LCM Rules")
@@ -784,7 +789,7 @@ public class SwaggerDocumentationConfig {
 	@Bean
 	public OpenApiCustomizer rpm685OpenAPI() {
 		return openApi -> openApi
-				.specVersion( SpecVersion.V30 ) 
+				.specVersion( SpecVersion.V30 ).addSecurityItem(new SecurityRequirement().addList("security_auth")) 
 				.info(new Info()
 						 .title("TMF 685 Resource Pool Management")
 						 .description("## TMF API Reference: TMF685 Resource Pool Management   ### Release : 18.0 "
